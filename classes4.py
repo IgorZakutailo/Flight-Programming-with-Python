48699a248f438bae8055468385754fe7aa50f006
IgorZakutailo <67501631+IgorZakutailo@users.noreply.github.com>
2020-06-27 15:52:07 +0300

    Update classes4.py

--- classes4.py
+++ classes4.py
@@ -1,4 +1,5 @@
 class Flight:
+
     counter = 1  # счетчик, который позволит мне пронумеровать каждый из моих рейсов
 
     def __init__(self, origin, destination, duration):
@@ -20,15 +21,45 @@ class Flight:
         self.destination = destination
         self.duration = duration
 
-        def print_info(self):
-            print(f"Flight origin: {self.origin}")
-            print(f"Flight destination: {self.destination}")
-            print(f"Flight duration: {self.duration}")
-            print()
-            print("Passengers:")
-            for passenger in self.passengers:
-                print(f"{passenger.name}")
-                def add_passenger(self, p):
-                    self.passengers.append(p)
-                    p.flight_id = self.id
-    
+    def print_info(self):
+        print(f"Flight origin: {self.origin}")
+        print(f"Flight destination: {self.destination}")
+        print(f"Flight duration: {self.duration}")
+
+        print()
+        print("Passengers:") #распечатываем список пассажиров
+        for passenger in self.passengers:
+            print(f"{passenger.name}")
+
+    def delay(self, amount):
+        self.duration += amount
+
+    def add_passenger(self, p): # p - это и есть имя пассажира
+        self.passengers.append(p) # добавляем пассажира
+        p.flight_id = self.id # устанавливаем идентификатор рейса пассажира
+        # мы обновляем объект Пассажир, чтобы отслеживать, какой рейс с которым он ассоциируется
+
+
+class Passenger:
+
+    def __init__(self, name):
+        self.name = name  # Всё, что сейчас делает класс Пассажиров, это отслеживание своего собственного имени.
+
+
+def main():
+
+    # Create flight.
+    f1 = Flight(origin="New York", destination="Paris", duration=540)
+
+    # Create passengers
+    alice = Passenger(name='Alice')
+    bob = Passenger(name='Bob')
+
+    # Add passengers.
+    f1.add_passenger(alice)
+    f1.add_passenger(bob)
+
+    f1.print_info()
+
+if __name__ == "__main__":
+    main()
